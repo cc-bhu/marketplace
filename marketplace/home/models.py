1c7a99441ecf39a548aec6220c05a8c89787b228
udit kumar agrawal <2001uditagrawal@gmail.com>
2023-07-10 10:29:18 +0530

    add product model add pillow to dependies

--- marketplace/home/models.py
+++ marketplace/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    product_image = models.ImageField(upload_to='product_images/', blank=True, null=True)
