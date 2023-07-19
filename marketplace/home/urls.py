df2ab5074fad3722b49ba22feb985c1938e11af0
udit kumar agrawal <2001uditagrawal@gmail.com>
2023-07-19 22:03:29 +0530

    update products

--- marketplace/home/urls.py
+++ marketplace/home/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .form import ProductForm
-from .views import addproduct, root
+from .views import addproduct, products, root
 
 app_name = "home"
 
 urlpatterns = [
     path("", root, name="root"),
     path("addproduct", addproduct, name="addproduct"),
+    path("products", products, name="products"),
 ]
